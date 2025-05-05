a6f0ec684d3cb0ec33b3dfcd54f11a515f63d4b4
Devika <devikaupendranr@gmail.com>
2025-05-05 10:10:15 +0530

    added favicons and assets

---
+++ client/src/assets/assets.js
@@ -0,0 +1,12 @@
+import logo from '../assets/images/logo.png'
+import loginBg from '../assets/images/login-bg.jpg'
+import homeVideo from '../assets/images/home.mp4'
+
+
+
+export const assets = {
+    logo,
+    loginBg,
+    homeVideo
+}
+
