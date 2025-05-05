8a273eca2ed3a710fe6930951532b286db63145d
Devika <devikaupendranr@gmail.com>
2025-05-05 10:31:11 +0530

    added navbar in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import LandingPage from './pages/LandingPage'
 import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import Navbar from './components/Navbar'
 
 const App = () => {
     return (
         <div>
+            <Navbar/>
             <Routes>
                 <Route path='/' element={<LandingPage />} />
                 <Route path='/home' element={<Home />} />
